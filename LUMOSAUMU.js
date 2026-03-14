271abf8fd10022a9a0ad8d1df23588f1133f3492
kigongoleo4 <kigongoleo4@gmail.com>
2026-03-14 19:50:04 +0300

    Add files via upload

---
+++ LUMOSAUMU.js
@@ -0,0 +1,28 @@
+function toggleMenu(){
+
+document.getElementById("menu").classList.toggle("show");
+
+}
+
+
+/* HERO SLIDER */
+
+let slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+function slider(){
+
+slides.forEach(slide => slide.classList.remove("active"));
+
+index++;
+
+if(index == slides.length){
+index = 0;
+}
+
+slides[index].classList.add("active");
+
+}
+
+setInterval(slider,4000);
